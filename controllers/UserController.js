d2360fc79600af61dd98d69848e5976d0590a933
Angelos Ioannou <t704@FRA-1.local>
2018-04-17 12:54:27 +0200

    JWT Authentication - needs testing

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var router = express.Router();
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
 var User = require("../models/User");
 
 router.get("/",function(req,res){
@@ -9,18 +12,7 @@ router.get("/",function(req,res){
     });
 });
 
-router.post("/",function(req,res){
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    },function(err,user){
-        if(err) return res.status(500).send("Couldn't create User");
-        res.status(200).send(user);
-    });
-});
-
-router.put("/",function(req,res){
+router.put("/update",function(req,res){
     User.findByIdAndUpdate(req.body.id,{
         name: req.body.name,
         email: req.body.email,
@@ -31,10 +23,27 @@ router.put("/",function(req,res){
     });
 });
 
-router.delete("/",function(req,res){
+router.delete("/delete",function(req,res){
     User.findByIdAndRemove(req.body.id,function(err,user){
         if(err) return res.status(500).send("Couldn't delete User");
         res.status(200).send("User "+user.name+" was deleted");
+    });
+});
+
+router.post("/register",function(req,res){
+
+    var hashedPassword = bcrypt.hashSync(req.body.password,8);
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    },function(err,user){
+        if(err) return res.status(500).send("User could not be created");
+        var token = jwt.sign({id: user._id}, process.env.SECRETKEY, {
+            expiresIn: 86400 // 24 Hours
+        });
+        res.status(200).send({auth: true, token: token});
     });
 });
 
